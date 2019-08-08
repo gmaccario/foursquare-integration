b3f872f9b7e6e1c112042a8399be1e2adc62ab8b
gmaccario <g_maccario@hotmail.com>
2019-08-08 13:51:24 +0200

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -5,6 +5,8 @@ if ( !defined( 'WP_UNINSTALL_PLUGIN' ))
     die;
 }
 
+include('include/constants.php');
+
 delete_option( FOURSQUARE_INTEGRATION_OPT_DEBUG );
 delete_option( FOURSQUARE_INTEGRATION_OPT_CLIENT_ID );
 delete_option( FOURSQUARE_INTEGRATION_OPT_SECRET_KEY );
